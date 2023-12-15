2942b5d850fcfd14039d3f9c12f0891b9c5125f1
AndriKovalchuk <143098639+AndriKovalchuk@users.noreply.github.com>
2023-12-15 12:03:35 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,111 +1,62 @@
-from datetime import datetime, timedelta
-from rich.console import Console
-import re
+from datetime import datetime
 
-console = Console()
 
 class Contact:
-    def __init__(self, name, address, phone, email, birthday):
+    def __init__(self, name, address, phone_number, email, birth_date):
         self.name = name
         self.address = address
-        self.phone = phone
+        self.phone_number = phone_number
         self.email = email
-        self.birthday = birthday
+        self.birth_date = datetime.strptime(birth_date, '%d.%m.%Y').date()
 
-class Note:
-    def __init__(self, text, tags):
-        self.text = text
-        self.tags = tags
 
 class PersonalAssistant:
     def __init__(self):
         self.contacts = []
-        self.notes = []
 
-    def is_valid_phone(self, phone):
-        # Перевірка правильності формату номера телефону
-        # Допустимі формати: +380501234567, 050-123-45-67, 0501234567, (050)123-45-67
-        phone_pattern = re.compile(r'^\+?\d{1,3}?[-.\s]?\(?\d{1,4}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$')
-        return bool(re.match(phone_pattern, phone))
+    def add_contact(self, contact):
+        self.contacts.append(contact)
 
-    def is_valid_email(self, email):
-        # Перевірка правильності формату електронної пошти
-        email_pattern = re.compile(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-        return bool(re.match(email_pattern, email))
-
-    def add_contact(self, name, address, phone, email, birthday):
-        # Додавання нового контакту
-        if not self.is_valid_phone(phone):
-            console.print("[bold red]Помилка:[/bold red] Некоректний номер телефону.")
-            return
-
-        if not self.is_valid_email(email):
-            console.print("[bold red]Помилка:[/bold red] Некоректна електронна пошта.")
-            return
-        
-    
-
-    def list_contacts(self):
-        # Виведення списку контактів
-        pass
-
-    def search_contacts(self, query):
-        # Пошук контактів за запитом
-        pass
-
-    def edit_contact(self, contact_index, name, address, phone, email, birthday):
-        # Редагування контакту
-        pass
-
-    def delete_contact(self, contact_index):
-        # Видалення контакту
-        pass
+    def display_contacts(self):
+        if not self.contacts:
+            print('There are no contacts added.')
+        else:
+            for contact in self.contacts:
+                print(f"Name: {contact.name}, Address: {contact.address} Phone: {contact.phone_number}, Email: {contact.email}, Birth date: {contact.birth_date.strftime('%d.%m.%Y')})")
 
     def upcoming_birthdays(self, days):
-        # Виведення списку контактів, дні народження яких наближаються
-        pass
-
-    def add_note(self, text, tags):
-        # Додавання нової нотатки
-        pass
-
-    def list_notes(self):
-        # Виведення списку нотаток
-        pass
-
-    def search_notes(self, query):
-        # Пошук нотаток за запитом
-        pass
-
-    def edit_note(self, note_index, text, tags):
-        # Редагування нотатки
-        pass
-
-    def delete_note(self, note_index):
-        # Видалення нотатки
-        pass
-
-    def sort_notes_by_tags(self):
-        # Сортування нотаток за тегами
-        pass
-
-    def categorize_files(self, folder_path):
-        # Сортування файлів у зазначеній папці за категоріями
-        pass
-
-    def analyze_user_input(self, user_input):
-        if "додати контакт" in user_input.lower():
-            console.print("[green]Пропоную вам додати новий контакт. Використайте команду add_contact.[/green]")
-        elif "список контактів" in user_input.lower():
-            console.print("[green]Для виведення списку контактів використайте команду list_contacts.[/green]")
-        
-
+        today = datetime.today().date()
+        upcoming_birthdays = [contact for contact in self.contacts if (contact.birth_date - today).days == days]
+        if not upcoming_birthdays:
+            print(f'There are no upcoming birthdays in {days} days.')
         else:
-            console.print("[yellow]Не можу розпізнати вашу команду. Спробуйте ще раз.[/yellow]")
+            print(f'Upcoming birthdays in {days} days: ')
+            for contact in upcoming_birthdays:
+                print(f"Name: {contact.name}, Birth date: {contact.birth_date.strftime('%d.%m.%Y')}")
 
 
 assistant = PersonalAssistant()
 
 while True:
-    user_input = input("Введіть команду: ")
-    assistant.analyze_user_input(user_input)
+    user_input = input('Enter the command (add / display / birthdays / exit): ')
+    if user_input == 'exit':
+        break
+
+    elif user_input == 'add':
+        name = input('Enter the name: ')
+        address = input('Enter the address: ')
+        phone_number = input('Enter the phone number: ')
+        email = input('Enter the email: ')
+        birth_date = input('Enter the date of birth in the following format <dd.mm.yyyy>: ')
+
+        new_contact = Contact(name, address, phone_number, email, birth_date)
+        assistant.add_contact(new_contact)
+
+    elif user_input == 'display':
+        assistant.display_contacts()
+
+    elif user_input == 'birthdays':
+        days = int(input('Enter the number of days for upcoming birthdays: '))
+        assistant.upcoming_birthdays(days)
+
+
